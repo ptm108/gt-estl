3ea2830ea63b1e33d08d7e8f6e298e06516cb938
Tze Ming <e0311042@u.nus.edu>
2021-09-05 18:40:25 +0800

    Re init migrations

--- service/employees/models.py
+++ service/employees/models.py
@@ -6,6 +6,7 @@ from decimal import Decimal
 
 
 class Employee(models.Model):
+    public_id = models.CharField(max_length=255, unique=True)
     login = models.CharField(max_length=255, unique=True)
     name = models.TextField()
     salary = models.DecimalField(decimal_places=2, max_digits=30, validators=[MinValueValidator(Decimal('0.0'), message='Salary cannot be negative')])
@@ -13,4 +14,9 @@ class Employee(models.Model):
     class Meta:
         ordering = ['id']
     # end Meta
+
+    def save(self, *args, **kwargs):
+        if self.public_id is None:
+            self.public_id = f'e{self.id:04}'
+        return super(Employee, self).save(*args, **kwargs)
 # end class
--- service/employees/service.py
+++ service/employees/service.py
@@ -6,8 +6,27 @@ from .models import Employee
 
 
 @transaction.atomic
-def batch_process_csv(data):
-    if not data:
+def batch_process_csv(csv_file):
+
+    # file validation
+    if not csv_file:
         raise ValidationError(_('File not found'))
-    print(data)
+    if csv_file.size <= 0:
+        raise ValidationError(_('Empty file'))
+    if csv_file.name[-4:] != '.csv':
+        raise ValidationError(_('Invalid format'))
+    # end ifs
+
+    for row in csv_file:
+        data = row.strip().decode().split(',')
+        print(data)
+
+        e = Employee(
+            login=data[1],
+            name=data[2],
+            salary=data[3],
+        )
+        e.save()
+    # end for
+
 # end def
