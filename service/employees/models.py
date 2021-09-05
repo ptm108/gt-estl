fa9f197909962f19988cd03de7789c4e6f5b9701
Tze Ming <e0311042@u.nus.edu>
2021-09-05 15:29:55 +0800

    Update models

--- service/employees/models.py
+++ service/employees/models.py
@@ -3,13 +3,11 @@ from django.db import models
 import uuid
 
 class Employee(models.Model):
-    employee_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
-    id = models.CharField(max_length=255, unique=True)
     login = models.CharField(max_length=255, unique=True)
     name = models.TextField()
     salary = models.PositiveIntegerField()
 
     class Meta:
-        ordering = ['employee_id']
+        ordering = ['id']
     # end Meta
 # end class
