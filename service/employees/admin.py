dd6e47cbe23bbc1ab2f7e24ef9ed4bf5839e359d
Tze Ming <e0311042@u.nus.edu>
2021-09-05 15:07:24 +0800

    Init models

--- service/employees/admin.py
+++ service/employees/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Employee
+
+
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ("id", "login", "name", "salary")
+# end class
+
+admin.site.register(Employee, EmployeeAdmin)
--- service/employees/models.py
+++ service/employees/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+import uuid
+
+class Employee(models.Model):
+    employee_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
+    id = models.CharField(max_length=255, unique=True)
+    login = models.CharField(max_length=255, unique=True)
+    name = models.TextField()
+    salary = models.PositiveIntegerField()
+
+    class Meta:
+        ordering = ['employee_id']
+    # end Meta
+# end class
