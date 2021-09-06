cbc4e44f32bcf2761d29147c3a7abb1bdc043254
Tze Ming <e0311042@u.nus.edu>
2021-09-06 23:25:22 +0800

    Update urls

--- service/employees/urls.py
+++ service/employees/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import upload_csv_view, employee_view
+from .views import upload_csv_view, employee_view, single_employee_view
 
 urlpatterns = [
     # upload employee csv data
     path('users/upload', upload_csv_view, name='Upload csv data'),
     path('users', employee_view, name='Get/Create employee'),
+    path('users/<str:id>', single_employee_view, name='Get/Update/Delete single employee'),
 ]
