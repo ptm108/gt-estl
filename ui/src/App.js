9804997c444e613f1c775ef28c2d6a290c142801
Tze Ming <e0311042@u.nus.edu>
2021-09-07 22:24:57 +0800

    Init layout

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,5 +1,14 @@
-function App() {
-  return <div>Test</div>;
-}
+import React from "react";
+import { Redirect, Route, Switch } from "react-router-dom";
+import Dashboard from "./features/Dashboard";
+
+const App = () => {
+  return (
+    <Switch>
+      <Route exact path="/" component={Dashboard} />
+      <Redirect to="/" />
+    </Switch>
+  );
+};
 
 export default App;
