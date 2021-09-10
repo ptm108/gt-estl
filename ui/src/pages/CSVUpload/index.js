09445c181bf7a7f97fb920926ef492fc36a9836d
Tze Ming <e0311042@u.nus.edu>
2021-09-10 16:39:36 +0800

    Init simple file upload

--- ui/src/pages/CSVUpload/index.js
+++ ui/src/pages/CSVUpload/index.js
@@ -1,23 +1,67 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
+import { Button, Card, CardContent, Chip, makeStyles, Typography } from "@material-ui/core";
 import { Description } from "@material-ui/icons";
 import { DropzoneAreaBase } from "material-ui-dropzone";
 
 import PageTitle from "../../components/PageTitle";
 
+const useStyles = makeStyles((theme) => ({
+  cardRoot: {
+    margin: theme.spacing(1, 0),
+  },
+  fileRoot: {
+    display: "flex",
+    flexDirection: "column",
+    width: 200,
+    gap: theme.spacing(1.5),
+    [theme.breakpoints.down("sm")]: {
+      width: "100%",
+    },
+  },
+  button: {
+    backgroundColor: theme.palette.primary.dark,
+    color: "#FFF",
+    padding: theme.spacing(0.5),
+    "&:hover": {
+      backgroundColor: theme.palette.primary.main,
+    },
+  },
+}));
+
 const CSVUpload = () => {
+  const classes = useStyles();
+  const [files, setFiles] = useState([]);
+  console.log(files);
+
   return (
     <Fragment>
       <PageTitle title="Upload CSV" />
-      <div style={{ margin: "8px 0" }}>
-        <DropzoneAreaBase
-          acceptedFiles={[".csv"]}
-          Icon={Description}
-          filesLimit={1}
-          dropzoneText="Drag and drop the .csv file here or click"
-          onChange={(files) => console.log("Files:", files)}
-          onAlert={(message, variant) => console.log(`${variant}: ${message}`)}
-        />
-      </div>
+      <Card className={classes.cardRoot}>
+        <CardContent>
+          {files.length > 0 ? (
+            <div className={classes.fileRoot}>
+              <Chip label={files[0].file.name} onDelete={() => setFiles([])} color="secondary" />
+              <Button className={classes.button} variant="contained">
+                Upload
+              </Button>
+            </div>
+          ) : (
+            <DropzoneAreaBase
+              acceptedFiles={[".csv", "text/csv"]}
+              Icon={Description}
+              filesLimit={1}
+              dropzoneText={
+                <Typography variant="h5" color="textSecondary" style={{}}>
+                  Drag and drop the .csv file here or click
+                </Typography>
+              }
+              fileObjects={files}
+              onAdd={(files) => setFiles(files)}
+              onDelete={(files) => setFiles(files)}
+            />
+          )}
+        </CardContent>
+      </Card>
     </Fragment>
   );
 };
